ccca229fc46c0eebf47d7f9d893fd50d6350e9cf
felipesaldias <felipe.saldias@shinkansen.finance>
2026-04-06 14:54:49 -0300

    fix SwiftData migration - add default values to Memory fields

--- Luna/Models/Memory.swift
+++ Luna/Models/Memory.swift
@@ -5,11 +5,11 @@ import SwiftData
 class Memory {
     @Attribute(.externalStorage) var imageData: Data?
     @Attribute(.externalStorage) var videoData: Data?
-    var isVideo: Bool
-    var caption: String
-    var place: String
+    var isVideo: Bool = false
+    var caption: String = ""
+    var place: String = ""
     var milestoneId: String?
-    var createdAt: Date
+    var createdAt: Date = Date.now
 
     init(imageData: Data? = nil, videoData: Data? = nil, isVideo: Bool = false, caption: String = "", place: String = "", milestoneId: String? = nil, createdAt: Date = .now) {
         self.imageData = imageData
